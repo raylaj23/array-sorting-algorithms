615c84147174b6a2b8dd1d80974fffcfb8f65d10
Stefan Raileanu <steffy.raileanu@gmail.com>
2024-02-05 18:12:50 +0200

    Update bubble-sort.c file

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
-static void sort(void){
+static void sort(int v[100], int n){
 //add bubble sort alg
 }
 
 int main(void){
 
+int n, v[100];
+scanf("%d", &n);
+int i;
+for(i = 0; i < n; i++)
+	scanf("%d", &v[i]);
+
+sort(v, n);
+for(i = 0; i < n; i++)
+	printf("%d ", v[i]);
 return 0;
 }
